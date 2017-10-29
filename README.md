3eb7aed967d812cc5d37f0868b6656d235a04e64
Andrey <andnovator@gmail.com>
2017-10-29 23:24:51 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Joomla! 3.x plugin for change template and save template state for site
+========================================
+
+by [Andrey Novikov](https://github.com/AndNovAtor)
+
+## License
+
+This project has [MIT](./LICENSE.md) [licence](https://opensource.org/licenses/MIT).
---
+++ templateswitcher.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @copyright   Modified by: A.Novikov, 2017. Original: Copyleft (C) 2014 Ilhom666, Inc. All rights reserved.
+ * @license     GNU General Public License version 3 or later
+*/
+
+// no direct access
+defined( '_JEXEC' ) or die;
+
+class PlgSystemTemplateSwitcher extends JPlugin {
+    /**
+     * Switches template after turning on by input, or sets input for saving current template if template name is/was stored in cookies
+     * Also cleans some major caches: after changing template some components saved previous layout if cache is enabled on site and if previous template has layout override of some components
+     *
+     * @return  void
+     */
+    function onAfterInitialise() {
+        $app = JFactory::getApplication();
+        $input = $app->input;
+        $template_input = $input->getCmd( 'template', '' );
+        $template_cookie_var_name = 'current_template_name';
+        if ( $template_input !== '' ) {
+            // Clean cache "on template changed" only
+            // TODO: think about what cache needs to be cleaned
+            JFactory::getCache('com_content')->clean();
+            JFactory::getCache('com_contact')->clean();
+            JFactory::getCache('com_search')->clean();
+            JFactory::getCache('mod_articles_category')->clean();
+            JFactory::getCache('mod_articles_news')->clean();
+            JFactory::getCache('mod_breadcrumbs')->clean();
+            JFactory::getCache('mod_custom')->clean();
+            JFactory::getCache('mod_feed')->clean();
+            JFactory::getCache('mod_menu')->clean();
+            JFactory::getCache('mod_random_image_extended')->clean();
+            JFactory::getCache('mod_search')->clean();
+            JFactory::getCache('mod_templateselector')->clean();
+            // Store temlate name in cookies (for 365 days ~= 1 year)
+            setcookie($template_cookie_var_name, $template_input, time()+(3600 * 24 * 365), '/');
+        } elseif (isset($_COOKIE[$template_cookie_var_name])) {
+            $input->set( 'template', $_COOKIE[$template_cookie_var_name] );
+        }
+    }
+}
+
+
+
+?>
